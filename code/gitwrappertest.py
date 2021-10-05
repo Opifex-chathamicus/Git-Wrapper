7308af7caab978d2ed7979e3947eeafe1344e8ce
alXCS <alexanderlemnos@hotmail.com>
2021-10-05 20:30:31 +0300

    Edited gitwrappertest.py at code folder

--- code/gitwrappertest.py
+++ code/gitwrappertest.py
@@ -1,22 +1,75 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+"""
+Created on Tue Oct  5 02:29:58 2021
 
-import github3 
+@author: xcs
+"""
+import requests
+import base64
 
-gh = login(token='')
+tokenfd = open("/home/xcs/Documents/pythonprogramming/credtok.py",'r')
+TOKEN=tokenfd.readline().strip()
+tokenfd.close()
+print(TOKEN)
+API_URL="https://api.github.com"
+lsrepoprojurl="/repos/{owner}/{repo}/projects"
+teamurlrepo="/users/Opifex-chathamicus/repos"
+usrurlrepo="/users/alXCS/repos"
+usrurl="/users/alXCS"
+headers={
+        "Authorization":"Basic "+TOKEN,
+        "Accept":"*/*"
+        }
+r=requests.get(API_URL+usrurl,headers=headers)
+print(r.json())
+resp=r.json()
+me=resp['login']
+#print(me)
 
-me= gh.me()
+#####################################################################
+#######################GET REPO CONTENTS #############################
+
+r2=requests.get(API_URL+"/repos/Opifex-chathamicus/Konopzzzz/contents/modules/",headers=headers)
+#print(r2.json())
+counts=len(r2.json())
+resp2=r2.json()
+for i in range(counts):
+   print(resp2[i]['name'],":")
+   print(resp2[i]['sha'])
+   print("=========================================================")
+
+###########################################################################
+####################### GET FILE CONTENTS #################################
+
+headers2={
+        "Authorization":"Basic "+TOKEN,
+        "Accept":"application/vnd.github.v3+json"
+        }
+r3=requests.get(API_URL+"/repos/Opifex-chathamicus/Konopzzzz/contents/modules/dirlister.py",headers=headers2)
+print(r3.json())
+resp3=r3.json()
+print("======================= CONTENT ======================= ")
+print(resp3['content'])
+contentresp3=resp3['content']
+contentresp3=base64.b64decode(contentresp3)
+contentresp3=contentresp3.decode("UTF-8") #the base64 decoded data has to be converted from bytes to string
+print(contentresp3)
+
+###########################################################################
+####################### WRITE LOCALLY TO FILE #############################
+
+modname=resp3['name']
+modfd=open("/home/xcs/Desktop/"+modname,'w')
+modfd.write(contentresp3)
+modfd.close()
 
 
-print(me.name)
+###########################################################################
+####################### POST TO GITHUB ###################################
 
-print(me.login)
 
-print(me.followers_count)
 
-owner="Opifex-chathamicus"
-repository_name="Konopzzzz"
-short_repository = qh.repository(owner, repository_name)
-for short_repository in gh.repositories_by(owner):
-    full_repository = short_repository.refresh()
-print(full_repository)
+
+
+
